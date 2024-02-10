8e3ee2801ccaddf181dcc16e9bff1850a223e149
Phil Fuster <pafus23@protonmail.com>
2024-02-10 09:49:10 -0500

    Remove TanStackRouterVite from vite config that generates the routeTree.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
+	plugins: [react()],
 	css: {
 		transformer: 'lightningcss',
 	},
