e6d9fe9f7f2f59987fe4856e90610c176d4c7624
Philip Fuster <pafus23@protonmail.com>
2024-02-11 17:11:24 -0500

    change biome-ignore description

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.tsx';
-import './index.css';
 
-// biome-ignore lint: Forbidden Null Assertion - demoing
+// biome-ignore lint: Forbidden Null Assertion - root element is defined in index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
