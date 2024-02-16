5b82afed9ed513345c2a44a51e4e17fdd146f1b0
Philip Fuster <pafus23@protonmail.com>
2024-02-16 08:12:24 -0500

    Update exercise page text.

--- src/routes/exercise/exerciseIndex.lazy.tsx
+++ src/routes/exercise/exerciseIndex.lazy.tsx
@@ -7,7 +7,13 @@ export const Route = createLazyRoute('/exercise')({
 function Index() {
 	return (
 		<div>
-			<h1>Exercises!</h1>
+			<h1>Exercises</h1>
+			<p>
+				An <em>exercise</em>, is the training of the body to improve its function and enhance its
+				fitness. In Fitness Manager, you can create and manage exercises that you can then
+				incorporate in <em>workouts</em> (a group of exercises or multiple exercises performed in
+				one training session).{/* TODO: Add a link here to the Workouts page. */}
+			</p>
 			<ul>
 				<li>Push Ups</li>
 				<li>Sit Ups</li>
