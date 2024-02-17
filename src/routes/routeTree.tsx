2f6279528f949cf0f49b545efb8c3149586eb6eb
Philip Fuster <pafus23@protonmail.com>
2024-02-17 14:43:05 -0500

    Update root route name

--- src/routes/routeTree.tsx
+++ src/routes/routeTree.tsx
@@ -1,12 +1,13 @@
 import '../styles/style.css';
-import { exerciseRouteTree } from './exercise/routes';
+import { exerciseRouteTree,exerciseCreateRoute } from './exercise/routes';
 import { aboutRoute, roadmapRoute, rootIndexRoute } from './root';
-import { rootRoute } from './root/root';
+import { rootRoute } from './root/__root';
 
 export const routeTree = rootRoute.addChildren([
 	rootIndexRoute,
 	aboutRoute,
 	roadmapRoute,
+  exerciseCreateRoute,
 	exerciseRouteTree,
 ]);
 export { rootRoute };
