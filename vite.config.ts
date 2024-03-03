01963700327653b487c370c7ee39fde6c3bbb9b2
Philip Fuster <pafus23@protonmail.com>
2024-03-03 13:06:58 -0500

    Remove stylex from tsconfig.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths(), styleX()],
+	plugins: [react(), tsconfigPaths()],
 	css: {
 		transformer: "lightningcss",
 	},
