cae6b83af75776f264951c62a93ced8c494e5494
Philip Fuster <pafus23@protonmail.com>
2024-09-15 19:58:39 -0400

    fix type issue found by running typecheck

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.tsx';
+import App from './App.tsx';
 
 // biome-ignore lint: Forbidden Null Assertion - root element is defined in index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
