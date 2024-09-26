8452513a34ce7a18044f13228b50cfa24a4883b8
Philip Fuster <pafus23@protonmail.com>
2024-09-26 10:30:38 -0400

    Add testing (#10)
    ### Summary
    
    installed vitest and react testing library dependencies.
    
    configure vitest in vite.config.ts.
    
    add testing library jest-dom types and vitest global types to
    tsconfig.json.
    
    include the vitest setup script in the TS config.
    
    add package.json scripts to running tests, coverage, and running the
    vitest tesing UI.
    
    add coverage to .gitignore file.
    
    Add testing for the exercise create form.
    
    Add form error message when form is not valid and has been submitted.
    
    Change before form validation mode to `onTouched`.
    
    Added packageManager field to package.json and set to latest version of
    pnpm as of 2024-09-24.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -5,6 +7,12 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./vitest.setup.ts'],
+		css: true,
+	},
 	css: {
 		transformer: 'lightningcss',
 	},
